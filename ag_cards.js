ba8935b37b8c395c84861a3111be2c020c4c7301
Lyle Goldman <gizmobro@hotmail.com>
2026-04-30 20:35:33 -0400

    Finished 14.1.

---
+++ ag_cards.js
@@ -0,0 +1,46 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5, CSS3, and JavaScript 6th Edition
+   Tutorial 14
+   Tutorial Case
+
+   Author: Lyle Goldman
+   Date:   4/30/2026
+
+   Filename: ag_cards.js
+
+
+   Custom Object Classes
+   
+   pokerGame
+      The pokerGame object contains properties and methods for the game
+      of draw poker
+
+   pokerDeck
+      The pokerDeck object contains an array of poker cards and methods
+      for shuffling and drawing cards from the deck.
+
+   pokerHand
+      The pokerHand object contains an array of poker cards drawn from a
+      poker deck. The methods associated with the object include the ability 
+      to calculate the value of the hand and to mark cards to be discarded,
+      replaced with new cards from a poker deck.
+
+   pokerCard
+      The pokerCard object contains properties and methods associated with
+      individual poker cards including the card rank, suit, and value.
+   
+	
+*/
+
+/* The pokerGame object. */
+var pokerGame = {
+    currentBank: null,
+    currentBet: null,
+
+    placeBet: function() {
+        this.currentBank -= this.currentBet;
+        return this.currentBank;
+    }
+};
---
+++ ag_poker.js
@@ -0,0 +1,84 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5, CSS3, and JavaScript 6th Edition
+   Tutorial 14
+   Tutorial Case
+
+   Author: Lyle Goldman
+   Date:   4/30/2026
+   
+   Filename: ag_poker.js
+
+*/
+
+window.addEventListener("load", playDrawPoker);
+
+function playDrawPoker() {
+    var dealButton = document.getElementById("dealB");
+    var drawButton = document.getElementById("drawB");
+    var standButton = document.getElementById("standB");
+    var resetButton = document.getElementById("resetB");
+    var handValueText = document.getElementById("handValue");
+    var betSelection = document.getElementById("bet");
+    var bankBox = document.getElementById("bank");
+
+    // Set the initial values of the pokerGame object.
+    pokerGame.currentBank = 500;
+    pokerGame.currentBet = 25;
+ 
+    bankBox.value = pokerGame.currentBank;
+    betSelection.onchange = function(e) {
+        pokerGame.currentBet = parseInt(e.target.options[e.target.selectedIndex].value);
+    };
+
+    // Restart the game when the Reset button is clicked.
+    resetButton.addEventListener("click", function() {
+        pokerGame.currentBank = 500;
+        bankBox.value = pokerGame.currentBank;
+        enableObj(dealButton);
+        enableObj(betSelection);
+        disableObj(drawButton);
+        disableObj(standButton);
+    });
+
+    // Enable the Draw and Stand buttons after the deal.
+    dealButton.addEventListener("click", function() {
+        if(pokerGame.currentBank >= pokerGame.currentBet) {
+            disableObj(dealButton);
+            disableObj(betSelection);
+            enableObj(drawButton);
+            enableObj(standButton);
+            bankBox.value = pokerGame.placeBet();
+        }
+        else {
+            alert("Reduce the size of your bet.");
+        }
+    });
+
+    // Enable the Deal and Bet options when the current hand ends.
+    drawButton.addEventListener("click", function() {
+        enableObj(dealButton);
+        enableObj(betSelection);
+        disableObj(drawButton);
+        disableObj(standButton)
+    });
+    standButton.addEventListener("click", function() {
+        enableObj(dealButton);
+        enableObj(betSelection);
+        disableObj(drawButton);
+        disableObj(standButton)
+    })
+
+    // Disable Poker Button.
+    function disableObj(obj) {
+        obj.disabled = true;
+        obj.style.opacity = 0.25;
+    }
+    
+    // Enable Poker Button.
+    function enableObj(obj) {
+        obj.disabled = false;
+        obj.style.opacity = 1;
+    }
+}
